202d4ae073b9be2b45dd83b8f251c140842d75b0
antoninovaccarella <antoninovaccarella@outlook.com>
2023-06-02 00:52:05 +0200

    Update api.ts

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,6 +1,6 @@
 import {environment} from '../../environments/environment';
 
-export const BASE_API = environment.production ? 'https://api.book101.com/' : 'https://book101-cloud.uc.r.appspot.com/api/';
+export const BASE_API = environment.production ? 'https://api.book101.com/' : 'http://localhost:8080/api/';
 export const AUTH_API = BASE_API + 'auth/';
 export const PRODUCT_API = BASE_API + 'product/';
 export const USER_API = BASE_API + 'user/';
