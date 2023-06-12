a622900911763f9036e7a2dddd117d2f68b35ab3
antoninovaccarella <antoninovaccarella@outlook.com>
2023-06-12 12:57:54 +0200

    update my-orders

--- src/app/components/pages/my-orders/my-orders.component.ts
+++ src/app/components/pages/my-orders/my-orders.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Order } from 'src/app/models/order';
 import { CartService } from '../../shared/services/cart.service';
+import {Product} from '../../../models/product';
 
 @Component({
   selector: 'app-my-orders',
@@ -16,6 +17,39 @@ export class MyOrdersComponent implements OnInit {
 
   ngOnInit(): void {
     this.cartService.getOrders().subscribe(orders => this.orders = orders);
+  }
+
+  convertBase64ToBlob(base64String: string): Blob {
+    const byteCharacters = atob(base64String);
+    const byteArrays = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+      const slice = byteCharacters.slice(offset, offset + 512);
+      const byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+      const byteArray = new Uint8Array(byteNumbers);
+      byteArrays.push(byteArray);
+    }
+
+    return new Blob(byteArrays, { type: 'application/octet-stream' });
+  }
+
+
+  downloadBinaryFile(base64String: string, filename: string) {
+    const blob = this.convertBase64ToBlob(base64String);
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = filename;
+    link.click();
+  }
+
+
+  downloadPdf(product: Product) {
+    const base64String = product.pdf; // Sostituisci con il tuo effettivo file base64
+    const filename = product.name + '.pdf'; // Sostituisci con il nome del file desiderato
+    this.downloadBinaryFile(base64String, filename);
   }
 
 }
